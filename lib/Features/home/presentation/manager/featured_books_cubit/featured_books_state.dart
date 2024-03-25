c6686f8823c967964eb35a14580cd7b2197f756c
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-03-25 22:24:13 +0300

    add faetured books cubit states implement fetch featured books cubit

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -4,3 +4,17 @@ part of 'featured_books_cubit.dart';
 sealed class FeaturedBooksState {}
 
 final class FeaturedBooksInitial extends FeaturedBooksState {}
+
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errormMessage;
+
+  FeaturedBooksFailure(this.errormMessage);
+}
+
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+
+  FeaturedBooksSuccess(this.books);
+}
